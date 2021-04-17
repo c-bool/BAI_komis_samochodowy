1603deccc14bef0930945bac46e6fd1b87bfe698
c-bool <next199701@gmail.com>
2021-04-17 21:40:29 +0200

    new project structure files

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+
+<style>
+@import url("../node_modules/bootstrap/dist/css/bootstrap.min.css");
+html {
+  position: relative;
+  min-height: 100%;
+}
+body {
+  margin-bottom: 60px;
+  color: #000000ad;
+}
+button {
+  color: #000000ad;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <p class="text-muted">2021 Car Dealer</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 60px;
+  background-color: #41608c;
+}
+
+.container .text-muted {
+  margin: 20px 0;
+  color: white !important;
+}
+
+.footer > .container {
+  padding-right: 15px;
+  padding-left: 15px;
+}
+</style>
