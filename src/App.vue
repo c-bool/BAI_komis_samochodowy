a7d1cdc042993f52ab9fc3e82cdf369efcdb19a9
Daniel <daniel@Daniels-MacBook-Air.local>
2021-04-18 12:25:52 +0200

    testing commit

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import Navbar from "./components/Navbar.vue";
 
+
 export default {
   name: "App",
   components: {
