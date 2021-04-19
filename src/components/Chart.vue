d1eb18d3f9672d10b79d4f2871c9837b6cf19cc5
Michal Antos <michal.antos@techmo.pl>
2021-04-19 14:57:03 +0200

    Chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -5,18 +5,18 @@ import { Bar } from 'vue3-chart-v2'
 export default defineComponent({
   name: 'MonthlyChart',
   extends: Bar,
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    },
+    chartOptions: {
+      type: Object,
+      required: false
+    },
+  },
   mounted () {
-    // Overwriting base render method with actual data.
-    this.renderChart({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      datasets: [
-        {
-          label: 'GitHub Commits',
-          backgroundColor: '#f87979',
-          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-        }
-      ]
-    })
+    this.renderChart(this.chartData, this.chartOptions)
   }
 })
 </script>
--- src/components/ChartsAndDiagrams.vue
+++ src/components/ChartsAndDiagrams.vue
@@ -1,22 +1,51 @@
   
 <template>
-    <Chart />
-
+  <Chart v-bind:chartData="chartData" :chartOptions="chartOptions" />
 </template>
 
 <script>
-
-
-import Chart from './Chart.vue'
-export default {
-    components: {
-    Chart
-  },
- props: { 
+import Chart from "./Chart.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
     cars: {
       type: undefined,
       required: true,
     },
   },
-}
+
+  components: {
+    Chart,
+  },
+  data() {
+    return {
+      chartOptions: {
+        height: 10
+      },
+      chartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ],
+        datasets: [
+          {
+            label: "GitHub Commits",
+            backgroundColor: "#f87979",
+            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+          },
+        ],
+      },
+    };
+  },
+});
 </script>
